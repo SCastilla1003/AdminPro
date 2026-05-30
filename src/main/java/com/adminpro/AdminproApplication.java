afe07a6f2b44160346b1a4498c3ce4d38a5738b4
SCastilla1003 <scastilla1003@gmail.com>
2026-05-30 18:38:14 -0500

    Add visible startup log showing which storage backend is active (S3 vs Local)

--- src/main/java/com/adminpro/AdminproApplication.java
+++ src/main/java/com/adminpro/AdminproApplication.java
@@ -1,15 +1,26 @@
 package com.adminpro;
 
+import com.adminpro.service.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class AdminproApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(AdminproApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdminproApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner showStorageBackend(StorageService storageService) {
+		return args -> log.info(">>> STORAGE BACKEND ACTIVE: {}", storageService.getClass().getSimpleName());
+	}
 }
